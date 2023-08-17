3e8858160eca0d012ccb770b0b6bc6c49e79a266
Nan <asus.bg87@gmail.com>
2023-08-17 22:49:27 +0700

    page 82

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,8 @@
-let  express = require('express')
+let express = require('express')
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
 
 const app = express()
 
@@ -12,9 +15,9 @@ app.get('/status', function (req, res ){
     res.send('Hello nodejs server')
 })
 
-app.get('/hello/:name', function (req,res) {
-    console.log('hello - ' + req.params.name)
-    res.send('sey hello with ' + req.params.name)
+app.get('/hello/:person', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
 })
 
 // get user by id
@@ -42,8 +45,14 @@ app.delete('/user/:userId', function (req, res) {
     res.send('ทำการลบผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name))
 })
 
-let port = 8081
+app.post('/hello', function (req, res) {
+    res.send('OK you post - ' + req.body.name)
+})
 
-app.listen(port, function () {
-    console.log('server running on ' + port)
+let port = process.env.PORT || config.port
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
